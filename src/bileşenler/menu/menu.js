0bf21755a9b10157f72148a285c3192c72991fb0
mcmerdanoglu <mcmerdanoglu@gmail.com>
2023-03-25 16:40:20 +0300

    Tamamlandı.

--- src/bileşenler/menu/menu.js
+++ src/bileşenler/menu/menu.js
@@ -1,13 +1,13 @@
-import './Menu.less'
+import "./Menu.less";
 
 // Kullanacağımız veri bu, üzerinde çalışın ama henüz hiçbir şeyi değiştirmeyin.
 let menuElemanlari = [
-  'Gündem',
-  'Dünya',
+  "Gündem",
+  "Dünya",
   "Ekonomi",
-  'Yazarlar',
-  'Burç Yorumları',
-  'Diğer'
+  "Yazarlar",
+  "Burç Yorumları",
+  "Diğer",
 ];
 
 /*
@@ -34,3 +34,32 @@ let menuElemanlari = [
 
   Adım 6: 'menuYapici' fonksiyonunu ve 'menuElemanlari' dizisini kullanarak menüyü oluşturun, ve döndürülen menüyü header'e ekleyin.
 */
+
+function menuYapici(menuList) {
+  const bodyEl = document.getElementsByClassName("header")[0]; // html'den header elemanını seçiyoruz. Biz list döndüğünden 0'ncısı seçiyoruz
+  const menuContainer = document.createElement("div"); // div tipinde bir element oluşturuyoruz <div></div>
+  const elements = document.createElement("ul"); // ul tipinde bir element oluşturduk  <ul></ul>
+  menuContainer.classList.add("menu"); // 60'ncı satırda oluşturduğumuz  <div class="menu"></div>
+  menuContainer.appendChild(elements); // 62'dekini 60'a eleman  <div class="menu"><ul></ul></div>
+
+  menuList.map((liItem) => {
+    // fonksiyonda tanımladığımız parametre için, map dönüyoruz
+    const menuItem = document.createElement("li"); // li elemanı oluşturuyoruz <li></li>
+    menuItem.textContent = liItem; // 67'da oluşturduğumuz li içine "liItem"dan gelen metni <li>olalala</li>
+    elements.appendChild(menuItem); // 61'deki elements elemanına li elemanını cocuk olarak veriyoruz.
+    // <div class="menu"><ul><li>oallalalal</li></ul></div>
+  });
+  // <div class="menu"><ul><li>oallalalal</li><li>oallalalal</li></ul></div>
+  // <div class="menu"><ul><li>oallalalal</li><li>oallalalal</li><li>oallalalal</li></ul></div>
+  // <div class="menu"><ul><li>oallalalal</li><li>oallalalal</li><li>oallalalal</li><li>oallalalal</li></ul></div>
+  // <div class="menu"><ul><li>oallalalal</li><li>oallalalal</li><li>oallalalal</li><li>oallalalal</li><li>oallalalal</li></ul></div>
+
+  bodyEl.appendChild(menuContainer); // menüyü header içine ekliyor
+  const button1 = document.querySelector(".menu-button"); // ekrandan menü butonunu seçiyor
+  button1.addEventListener("click", () => {
+    // buton1'e tıklanma durumunu dinlemeye başlıyoruz
+    menuContainer.classList.toggle("menu--open"); // <div class="menu menu--open"></div>
+  });
+}
+
+menuYapici(menuElemanlari); // fonksiyon ile, yukarıdaki data listesi ile çalıştırıyor.
